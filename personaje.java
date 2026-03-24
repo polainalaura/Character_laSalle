22961adcc920c333a4ea559b186eaf6dfeefe4cf
polainalaura <92717040+polainalaura@users.noreply.github.com>
2026-03-24 20:45:57 +0100

    Update personaje.java

--- personaje.java
+++ personaje.java
@@ -1,18 +1,15 @@
-
+//creacion del personaje
 public class Personaje {
 
     private String Name;
     private int level;
     private int experience;
-
-
+//datos para la clase de personaje
     public Personaje(String name, int level, int experience) {
         this.name = name;
         this.level = level;
         this.experience = experience;
     }
-
-    
 
     public String getName() {
         return name;
@@ -24,12 +21,12 @@ public class Personaje {
 
 
     public int getExperience() {
-        return level;
+        return experience;
     }
-
+    
+//salida de datos
     @Override
     public String toString() {
-
-        return nombre + " | Nivel: " + level + " | Exp: " + experience;
+        return name + " | Nivel: " + level + " | Exp: " + experience;
     }
 }
